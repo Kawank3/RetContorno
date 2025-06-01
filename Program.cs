339b936730758e078e737c3d6fc4efd0c4584d42
Kawan Weber <kawan_weber@hotmail.com>
2025-06-01 13:48:54 -0300

    Contorno v1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Digite a largura (1 a 10): ");
+int largura = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a altura (1 a 10): ");
+int altura = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nContorno do retângulo:\n");
+
+for (int i = 0; i < altura; i++)
+{
+    if (i == 0 || i == altura - 1)
+    {
+        // Primeira ou última linha: só asteriscos
+        Console.WriteLine(new string('*', largura));
+    }
+    else
+    {
+        // Linhas do meio: * + espaços + *
+        Console.Write("*");
+        Console.Write(new string(' ', largura - 2));
+        if (largura > 1)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
